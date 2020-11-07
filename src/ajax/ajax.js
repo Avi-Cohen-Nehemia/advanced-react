83d8c4c57172bdc630f4a9f73e770d15eeedaa58
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-11-07 16:12:19 +0000

    complete GET specific article exercise

--- src/ajax/ajax.js
+++ src/ajax/ajax.js
@@ -1,12 +1,12 @@
 // import the axios configuration file in order to use it
 import axios from './axios'
 
-// Promise is an object we recieve after making a request and while waiting for a response fron the API
-// .then() is a way for us to write and execute code only after receiving a response from the API
-// axios .then() provides the 'response' object which contains all the data sent by the API.
-// for example: axios.get('/articles').then((response) => console.log(response))
-// this response object can be destrcutured to { data } and that what we would usually work with
-// .catch() is a way for us to handle errors in case out request failed.
+/* Promise is an object we recieve after making a request and while waiting for a response fron the API
+.then() is a way for us to write and execute code only after receiving a response from the API
+axios .then() provides the 'response' object which contains all the data sent by the API.
+for example: axios.get('/articles').then((response) => console.log(response))
+this response object can be destrcutured to { data } and that what we would usually work with
+.catch() is a way for us to handle errors in case out request failed. */
 axios.get('/articles')
     .then(({ data }) => console.log(data.data))
     .catch((error) => {
@@ -26,12 +26,18 @@ axios.post('/articles', {
 
 // POST exercise
 // Create a new article using axios and then console.log() its ID
-axios.post('/articles', {
-    title: 'My Second Article',
-    content: 'This is nice',
-    tags: ['second', 'post']
-}).then(({ data }) => {
-    console.log(data.data.id)
-}).catch(() => {
-    console.log('Oops... Somthing went wrong!')
-})
+// axios.post('/articles', {
+//     title: 'My Second Article',
+//     content: 'This is nice',
+//     tags: ['second', 'post']
+// }).then(({ data }) => {
+//     console.log(data.data.id)
+// }).catch(() => {
+//     console.log('Oops... Somthing went wrong!')
+// })
+
+// GET exercise
+// Fetch one of your articles from the API and console.log() it
+axios.get('/articles/2')
+    .then(({ data }) => console.log(data.data))
+    .catch(() => console.log('Oops... Somthing went wrong!'))
