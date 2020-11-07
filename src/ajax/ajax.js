be9559c370fe1f29e5ef27cc56ac51754d812a7f
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-11-07 15:43:55 +0000

    add notes about promises and .then()

--- src/ajax/ajax.js
+++ src/ajax/ajax.js
@@ -1,9 +1,10 @@
 // import the axios configuration file in order to use it
 import axios from './axios'
 
-// .then() is a way for us to make code execute only after receiving a response
-// axios provides the 'response' object which contains the data we requested.
-// axios.get('/articles').then((response) => console.log(response))
+// Promise is an object we recieve after making a request and while waiting for a response fron the API
+// .then() is a way for us to write and execute code only after receiving a response from the API
+// axios .then() provides the 'response' object which contains all the data sent by the API.
+// for example: axios.get('/articles').then((response) => console.log(response))
 // this response object can be destrcutured to { data } and that what we would usually work with
 axios.get('/articles').then(({ data }) => console.log(data.data))
 
