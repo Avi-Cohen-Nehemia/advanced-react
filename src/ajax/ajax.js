7dc05d24619fec850bfb57a468b71303ff697520
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-11-07 15:37:45 +0000

    add notes about promises and .then()

--- src/ajax/ajax.js
+++ src/ajax/ajax.js
@@ -1,7 +1,11 @@
 // import the axios configuration file in order to use it
 import axios from './axios'
 
-axios.get('/articles')
+// .then() is a way for us to make code execute only after receiving a response
+// axios provides the 'response' object which contains the data we requested.
+// axios.get('/articles').then((response) => console.log(response))
+// this response object can be destrcutured to { data } and that what we would usually work with
+axios.get('/articles').then(({ data }) => console.log(data.data))
 
 // POST
 // takes 2 argumants, url to post to and object of data to post
@@ -12,3 +16,4 @@ axios.post('/articles', {
     tags: ['article', 'coding']
 })
 */
+ 
