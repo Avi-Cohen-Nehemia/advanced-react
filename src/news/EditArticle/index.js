89989b182440a83a227da3d6051df791f9901de1
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-11-15 17:56:16 +0000

    api requstes using components - edit article excerise

---
+++ src/news/EditArticle/index.js
@@ -0,0 +1,104 @@
+import React, { Component } from 'react'
+import axios from './../../ajax/axios'
+
+class EditArticle extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            title: '',
+            content: '',
+            tags: ''
+        }
+
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    componentDidMount() {
+        const { id } = this.props
+
+        axios.get(`/articles/${id}`)
+        .then(({ data }) => {
+            this.setState({
+                title: data.data.title,
+                content: data.data.content,
+                tags: data.data.tags.reduce((acc, tag) => acc + tag + ', ', '')
+            })
+        })
+    }
+
+    handleChange(e, input) {
+        let change = {}
+        let value = e.currentTarget.value
+        change[input] = value
+        this.setState(change)
+    }
+
+    handleSubmit(e) {
+        e.preventDefault()
+
+        const { id } = this.props
+
+        axios.put(`/articles/${id}`, {
+            title: this.state.title,
+            content: this.state.content,
+            tags: this.state.tags.split(', ')
+        })
+        .then(({ data }) => {
+            console.log(data.data)
+            this.setState({
+                title: '',
+                content: '',
+                tags: ''
+            })
+        })
+        .catch((error) => console.log(error.response))
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <div className='form-row'>
+                    <div className='form-group col-md-4'>
+                        <label forhtml={'Title'}>{'Title'}</label>
+                        <input
+                            id={'Title'}
+                            value={this.state.title}
+                            onChange={(e) => this.handleChange(e, 'title')}
+                            className='form-control'
+                            type='text'
+                        />
+                    </div>
+                </div>
+                <div className='form-row'>
+                    <div className='form-group col-md-4'>
+                        <label forhtml={'Content'}>{'Content'}</label>
+                        <input
+                            id={'Content'}
+                            value={this.state.content}
+                            onChange={(e) => this.handleChange(e, 'content')}
+                            className='form-control'
+                            type='text'
+                        />
+                    </div>
+                </div>
+                <div className='form-row'>
+                    <div className='form-group col-md-4'>
+                        <label forhtml={'Tags'}>{'Tags'}</label>
+                        <input
+                            id={'Tags'}
+                            value={this.state.tags}
+                            onChange={(e) => this.handleChange(e, 'tags')}
+                            className='form-control'
+                            type='text'
+                        />
+                    </div>
+                </div>
+                <button>{'Submit'}</button>
+            </form>
+        )
+    }
+}
+
+export default EditArticle
