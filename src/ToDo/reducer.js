b9cce053dacd0601ed2be7c76426167619a42514
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-12-12 17:52:29 +0000

    solve ToDo list addItem exercise

---
+++ src/ToDo/reducer.js
@@ -0,0 +1,9 @@
+export const addItem = (state, { value }) => {
+    return {
+        ...state,
+        items: [
+            ...state.items,
+            { task: value, completed: false }
+        ]
+    }      
+}
